6d21b2ec7e0ad3d10a1718d51da5542540666197
WangXiaoDong <514587649@qq.com>
2017-12-27 10:47:54 +0800

    Initial Commit

---
+++ TrainingDesignPatterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrainingDesignPatterns
+//
+//  Created by wxd on 2017/12/27.
+//  Copyright © 2017年 wxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TrainingDesignPatterns/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TrainingDesignPatterns
+//
+//  Created by wxd on 2017/12/27.
+//  Copyright © 2017年 wxd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
